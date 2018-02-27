f30c7b19adb180a9d1a7099f56ad935d83478ba4
Ishtar Perez <isperez@csumb.edu>
2018-02-27 03:47:59 +0000

    Lab3

---
+++ silverJack.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html>
+    
+<!-- All styles and javascript go inside the head -->
+    <head>
+        <title>Lab 3</title>
+        <meta charset="utf-8" />
+        <style>
+            @import url("css/styles.css");
+            
+        </style>
+        <link href="https://fonts.googleapis.com/css?family=Pacifico" rel="stylesheet">
+      
+        <!--<link rel="css/styles" rel="stylesheet" text="text/css" /> -->
+    </head>
+
+<!-- This is the body -->
+<body>
+    <header>
+        <h1>Silver Jack</h1>
+    </header>
+    
+    <!-- Created a <div> to add the images to -->
+    <div class="box">
+        <!-- The images used -->
+        <h2>Wonder Woman</h2>
+        <img src="img/wonderWoman.png" alt="Picture of  wonder woman" border="5px" />
+        <br>
+        <br/>
+        
+        <h2>The Flash</h2>
+        <img src="img/flash.png" alt="Picture of the flash" border="5px" />
+        
+        <br>
+        <br/>
+        <h2>Superman</h2>
+        <img src="img/superman.png" alt="Picture of superman" border="5px"/>
+        
+        <br>
+        <br/>
+        <h2>Batman </h2>
+        <img id="first" src="img/batman.png" alt="Picture of Batman" border="5px" />
+        
+        <br>
+        <br/>
+         <!-- Creating a <hr> -->
+         <div>
+             <hr width="100%">
+         </div>
+        
+        
+    </div>
+   
+    
+    
+</body>
+</html>
