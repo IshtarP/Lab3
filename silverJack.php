72be77d172f508e90aab9a6f9b22425c1efa4593
Abraham De Alba <adealba@csumb.edu>
2018-02-28 23:30:49 +0000

    my part

---
+++ silverJack.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<html>
+    
+<!-- All styles and javascript go inside the head -->
+    <head>
+        <title>Lab 3</title>
+        <meta charset="utf-8" />
+        <style>
+            @import url("css/styles.css");
+            
+        </style>
+        <link href="https://fonts.googleapis.com/css?family=Pacifico" rel="stylesheet">
+      
+    </head>
+
+<!-- This is the body -->
+<body>
+    <header>
+        <h1>Silver Jack</h1>
+    </header>
+    
+    <!-- Created a <div> to add the images to -->
+    <div class="box">
+        <!-- The images used -->
+         
+        <div class="card">
+        <h3>Wonder Woman</h3>
+        <img src="img/wonderWoman.png" alt="Picture of  wonder woman" border="5px" >
+        </div>
+        
+        <img id="cards" src="img/cards/clubs/8.png"  width="90">
+        <img id="cards" src="img/cards/clubs/6.png"  width="90" >
+        <img id="cards" src="img/cards/clubs/3.png"  width="90" >
+        <img id="cards" src="img/cards/spades/7.png"  width="90" >
+        <img id="cards" src="img/cards/diamonds/12.png" width="90" >
+        <div class="try" >
+            <h3 id="score">Score</h3> 
+        </div>
+        
+        
+       
+        <div class="card">
+        <h3>The Flash</h3>
+        <img src="img/flash.png" alt="Picture of the flash" border="5px" />
+        </div>
+        
+        <img id="cards" src="img/cards/clubs/5.png" width="90" >
+        <img id="cards" src="img/cards/diamonds/11.png" width="90" >
+        <img id="cards" src="img/cards/diamonds/10.png" width="90" >
+        <img id="cards" src="img/cards/clubs/9.png" width="90" >
+        <img id="cards" src="img/cards/spades/6.png" width="90" >
+        <!--<br>-->
+        <div class="try" >
+            <h3 id="score">Score</h3>
+        </div>    
+        
+        
+        <div class="card" >
+        <h3>Superman</h3>
+        <img src="img/superman.png" alt="Picture of superman" border="5px"/>
+        </div>
+        
+        <img id="cards" src="img/cards/hearts/7.png" width="90" >
+        <img id="cards" src="img/cards/diamonds/6.png" width="90" >
+        <img id="cards" src="img/cards/hearts/1.png" width="90" >
+        <img id="cards" src="img/cards/hearts/13.png" width="90" >
+        
+        <div class="try">
+            <h3 id="score">Score</h3>
+        </div>
+       <!-- <br>-->
+        
+        
+        <div class="card" >
+        <h3>Batman </h3>
+        <img  src="img/batman.png" alt="Picture of Batman" border="5px" />
+        </div>
+        
+        <img id="cards" src="img/cards/clubs/3.png" width="90" >
+        <img id="cards" src="img/cards/hearts/10.png" width="90" >
+        <img id="cards" src="img/cards/diamonds/8.png" width="90" >
+        <img id="cards" src="img/cards/clubs/4.png" width="90" >
+        <img id="cards" src="img/cards/clubs/1.png" width="90" >
+        <img id="cards" src="img/cards/clubs/10.png" width="90" >
+        
+        <div class="try" >
+            <h3 id="score">Score</h3>
+        </div>
+        
+    <!-- </div> -->
+    </div> 
+   
+    
+    
+</body>
+</html>
