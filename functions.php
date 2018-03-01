3371990b12c75a1c50796ca7783996ce1c2d3c9c
Ishtar Perez <isperez@csumb.edu>
2018-03-01 22:55:30 +0000

    Missing Timer

--- functions.php
+++ functions.php
@@ -1,6 +1,6 @@
 <?php
     
-    function displayHand($hand) {
+    function displayHand($hand, $suit) {
         
         $points = 0;
         $count = count($hand);
@@ -8,18 +8,18 @@
         for($i = 0; $i < $count; $i++) {
             
           
-            $cardvalue = ($hand[$i] % 13) + 1;
+            $cardvalue = ($suit[$i] % 13) + 1;
 
             
-            if($hand[$i] <= 13) {
+            if($suit[$i] <= 13) {
                 $cardSuit = "clubs";
               
             }
-            elseif($hand[$i] > 13 && $hand[$i] <= 26) {
+            elseif($suit[$i] > 13 && $suit[$i] <= 26) {
                 $cardSuit = "diamonds";
                 
             }
-            elseif($hand[$i] > 26 && $hand[$i] <= 39) {
+            elseif($suit[$i] > 26 && $suit[$i] <= 39) {
                 $cardSuit = "hearts";
                 
             }
@@ -27,7 +27,7 @@
                 $cardSuit = "spades";
                 
             }
-            $points += ($hand[$i] % 13) + 1;
+            $points += ($suit[$i] % 13) + 1;
             echo "<img src='./img/cards/$cardSuit/$cardvalue.png' alt=' Card: $cardSuit $cardvalue' />" ;
         }
         
@@ -38,8 +38,109 @@
         
         
     }
+     
+     $hands = array();
+     $suits = array();
+     $players = array(array("name" => "Wonder Woman", "linkToImage" => "img/wonderWoman.png"), 
+                     array("name" => "The Flash", "linkToImage" => "img/flash.png"), 
+                     array("name" => "Superman", "linkToImage" => "img/superman.png"), 
+                     array("name" => "Batman", "linkToImage" => "img/batman.png"));
+                     
+    shuffle($players);
+    $cards = array();
     
-
-
+    // add cards to our deck
+    for($i = 0; $i < 52; $i++) {
+        array_push($cards,$i);
+    }
+    
+    // so we don't repeat cards
+    shuffle($cards);
+    $index = 0;
+    
+     
+     // 4 times because we have 4 players
+     for($i = 0; $i < 4; $i++) {
+         
+         // how many cards current player will get
+        $numCards = 8;
+        $playerHand = array();
+        $suitValues = array();
+        
+        for($j = 0; $j < $numCards; $j++) {
+            
+            $suit = $cards[$index];
+            $cardvalue = ($cards[$index] % 13) + 1;
+            array_push($playerHand,$cardvalue);
+            array_push($suitValues,$suit);
+            $index += 1;
+            if(array_sum ($playerHand) >= 36 && array_sum($playerHand) <= 42)
+            {
+                break;
+            }
+            if(array_sum($playerHand) > 42)
+            {
+                break;
+            }
+        }
+        
+        array_push($hands, $playerHand);
+        array_push($suits, $suitValues);
+    }
+    
+    
+function displayWinner($hands,$players) {
+    $scores= array();
+    for($i = 0; $i < 4; $i++) {
+       
+       $score = array_sum($hands[$i]);
+        
+        array_push($scores,$score);
+        $score = 0;
+    }
+    
+    $maxScore;
+    $occurences = 0;
+    $index = 0;
+    $indexTie;
+    $winnerPoints = 0;
+    
+    for($j = 0; $j < 4; $j++) {
+        
+        if($scores[$j] == $maxScore) {
+            
+            $occurences++;
+            $indexTie = $j;
+        }
+        
+       if($scores[$j]<=42 && $scores[$j]>$maxScore)
+            {
+               $maxScore = $scores[$j];
+               $index = $j;
+            }
+    }
+        
+    for($i = 0; $i < 4; $i++) {
+        
+        if($scores[$i] != $maxScore) {
+            
+            $winnerPoints += $scores[$i];
+        }
+    }
+        
+    $outcome = "";
+    if($occurences >= 1) 
+                    {
+                        $outcome = $players[$index]["name"] . " and " . $players[$indexTie]["name"] . " won! The winners get: " . $winnerPoints . " points!";
+                    } 
+                    else 
+                    {
+                        $outcome = $players[$index]["name"]  . " Wins " . $winnerPoints . " points!";
+                    }
+    
+    echo $outcome;
+}
 
 ?>
+    
+
--- silverJack.php
+++ silverJack.php
@@ -3,46 +3,7 @@
 
  <?php 
  include('functions.php');
- include('ABEfunction.php');
- 
- $hands = array();
- 
- $players = array(array("name" => "Wonder Woman", "linkToImage" => "img/wonderWoman.png"), 
-                 array("name" => "The Flash", "linkToImage" => "img/flash.png"), 
-                 array("name" => "Superman", "linkToImage" => "img/superman.png"), 
-                 array("name" => "Batman", "linkToImage" => "img/batman.png"));
-                 
-shuffle($players);
-$cards = array();
-
-// add cards to our deck
-for($i = 0; $i < 53; $i++) {
-    array_push($cards,$i);
-}
-
-// so we don't repeat cards
-shuffle($cards);
-$index = 0;
-
- 
- // 4 times because we have 4 players
- for($i = 0; $i < 4; $i++) {
-     
-     // how many cards current player will get
-    $numCards = rand(4,6);
-    $playerHand = array();
-    
-    
-    for($j = 0; $j < $numCards; $j++) {
-        array_push($playerHand,$cards[$index]);
-        $index += 1;
-    }
-    
-    array_push($hands, $playerHand);
-  
-}
-
-?>
+ ?>
 
 <html>
     
@@ -65,15 +26,18 @@ $index = 0;
     </header>
     
     <!-- Created a <div> to add the images to -->
+    <div class="winner">
+        <h1 style="color:red;float:center;"><?php  displayWinner($hands,$players); ?> </h1>
+    </div>
     <div class="box">
-        <h1 style="color:red;float:right;"><?php  displayWinner($hands,$players); ?> </h1>
+        
         <!-- The images used -->
          
         <div class="card">
         <h3> <?php echo $players[0]["name"]; ?> </h3>
            
         <img src=<?php echo $players[0]['linkToImage']; ?> alt="Picture of  wonder woman" border="5px" >
-          <?php displayHand($hands[0]); ?>
+          <?php displayHand($hands[0],$suits[0]); ?>
           
          </div>
   
@@ -82,7 +46,7 @@ $index = 0;
         <div class="card">
         <h3><?php echo $players[1]["name"]; ?></h3>
         <img src=<?php echo $players[1]['linkToImage']; ?> alt="Picture of the flash" border="5px" />
-            <?php displayHand($hands[1]); ?>
+            <?php displayHand($hands[1],$suits[1]); ?>
         </div>
       
 
@@ -91,7 +55,7 @@ $index = 0;
         <div class="card" >
         <h3><?php echo $players[2]["name"]; ?></h3>
         <img src=<?php echo $players[2]['linkToImage']; ?> alt="Picture of superman" border="5px"/>
-            <?php displayHand($hands[2]); ?>
+            <?php displayHand($hands[2],$suits[2]); ?>
         </div>
         
         
@@ -101,15 +65,12 @@ $index = 0;
         <div class="card" >
         <h3><?php echo $players[3]["name"]; ?></h3>
         <img  src=<?php echo $players[3]['linkToImage']; ?> alt="Picture of Batman" border="5px" />
-            <?php displayHand($hands[3]); ?>
+            <?php displayHand($hands[3],$suits[3]); ?>
            
         </div>
      
         <br>
         
-        <div>
-            <hr>
-        </div>
         <!-- Add a <hr> see if it createas a bottom one -->
         
         <!-- create a <div> tag -->
